5e631304ab47bbbbef7cd66530f2a72580ad1365
Tylerwhitehat <70729291+Tylerwhitehat@users.noreply.github.com>
2020-10-27 20:05:26 -0400

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,20 @@
+var database;
+var position;
+var backgroundImage
+var form,player,game,playerCount
+var gameState=0
+
+function setup(){
+  database = firebase.database();
+  console.log(database);
+  createCanvas(500,500);
+game=new Game()
+game.getState()
+game.start()
+}
+
+function draw(){
+  
+  
+}
+
